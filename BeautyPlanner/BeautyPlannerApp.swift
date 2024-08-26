d5c0b8973dc121f32bc2828b2bf3b47303e4cc7b
MAizada <aizadamukhamedgaliyeva@gmail.com>
2024-08-27 00:57:41 +0500

    Initial Commit

---
+++ BeautyPlanner/BeautyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyPlannerApp.swift
+//  BeautyPlanner
+//
+//  Created by Aizada on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BeautyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
