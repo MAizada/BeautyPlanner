205e9a0798cd806fdf06b83dffab0318da647bde
MAizada <aizadamukhamedgaliyeva@gmail.com>
2024-08-27 13:47:09 +0500

    Initial commit

---
+++ BeautyPlanner/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  BeautyPlanner
+//
+//  Created by Aizada on 27.08.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let lightPeach = Color(UIColor(named: "LightPeach") ?? UIColor(red: 1.0, green: 0.93, blue: 0.91, alpha: 1.0))
+    static let lightBackground = Color(UIColor(named: "LightBackground") ?? UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0))
+}
---
+++ BeautyPlanner/WelcomeView.swift
@@ -0,0 +1,60 @@
+//
+//  WelcomeView.swift
+//  BeautyPlanner
+//
+//  Created by Aizada on 27.08.2024.
+//
+import SwiftUI
+
+struct WelcomeView: View {
+    @State private var navigateToNextView = false
+
+    var body: some View {
+        ZStack {
+            Color.lightPeach
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        navigateToNextView = true
+                    }) {
+                        Text("Skip")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.trailing, 40)
+                    }
+                }
+                .padding(.top, 30)
+
+                Spacer()
+
+                Text("Welcome to BeautyPlanner")
+                    .font(.custom("HelveticaNeue-Bold", size: 36))
+                    .foregroundColor(.pink)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+
+                Spacer()
+
+                Text("Your personal guide to beauty and self-care")
+                    .font(.custom("HelveticaNeue", size: 20))
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 50)
+            }
+
+//            NavigationLink(destination: NextView(), isActive: $navigateToNextView) {
+//                EmptyView()
+//            }
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
